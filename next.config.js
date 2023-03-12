f0d4e6f9e2ffba1b109ad2c5933b5d1c08872a7d
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-03-12 17:45:33 +0000

    delete all variables config

--- next.config.js
+++ next.config.js
@@ -4,16 +4,6 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
-  env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    MONGODB_URI: process.env.MONGODB_URI,
-    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-    NEXT_PUBLIC_CLOUDINARY_API_KEY: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
-    CLOUDINARY_SECRET: process.env.CLOUDINARY_SECRET,
-    CLOUDINARY_URL: process.env.CLOUDINARY_URL,
-    NEXT_PUBLIC_GOOGLE_ANALYTICS: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
-  },
   swcMinify: true,
 }
 
